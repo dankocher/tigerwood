f3e17fac8d585d49c16c115f6a3df4adf63a5777
Daniel Chernyshev <droque123@gmail.com>
2020-09-11 20:36:58 +0300

    Added Section 1

--- src/components/Sections/Sections.js
+++ src/components/Sections/Sections.js
@@ -2,28 +2,42 @@ import React from "react";
 import "./styles.scss";
 import formatText from "../../utils/formatText";
 
+import Section01 from "./Section01";
+
 import t from "../../translates";
 
+const sections = [1];
+
 class Sections extends React.Component {
+    //
+    // state = {
+    //     content: "test"
+    // }
 
-    state = {
-        content: "test"
-    }
-
-    componentDidMount() {
+    getTestSection = (amount = 100) => {
         let content = "";
-        for (let i = 0; i < 100; i++) {
-            content += t.lorem_ipsum + "\n"
+        for (let i = 1; i <= amount; i++) {
+            content += t.lorem_ipsum
+            if (i < amount) {
+                content += "\n"
+            }
         }
 
-        this.setState({content})
+        return content;
+    }
+
+    getSection = i => {
+        switch (i) {
+            case 1: return <Section01 key={`section-01`}/>
+            default: return formatText(this.getTestSection(100), "section --temporal")
+        }
     }
 
     render() {
         return (
             <div className={`sections`}>
                 <div className="content">
-                    {formatText(this.state.content, "temporal-div")}
+                    {sections.map(i => this.getSection(i))}
                 </div>
             </div>
         );
