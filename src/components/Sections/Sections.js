7495434a1b9cd2e045c9e467059f7bf29ed5e1a8
Daniel Chernyshev <droque123@gmail.com>
2020-09-17 15:45:55 +0300

    Added templates

--- src/components/Sections/Sections.js
+++ src/components/Sections/Sections.js
@@ -7,6 +7,12 @@ import Section02 from "./Section02";
 import Section03 from "./Section03";
 import Section04 from "./Section04";
 import Section05 from "./Section05";
+import Section06 from "./Section06";
+import Section07 from "./Section07";
+import Section08 from "./Section08";
+import Section09 from "./Section09";
+import Section10 from "./Section10";
+import Section11 from "./Section11";
 
 import t from "../../translates";
 
@@ -37,12 +43,12 @@ class Sections extends React.Component {
             case 3: return <Section03 key={`section-03`}/>
             case 4: return <Section04 key={`section-04`}/>
             case 5: return <Section05 key={`section-05`}/>
-            case 6: return <Section05 key={`section-06`}/>
-            case 7: return <Section05 key={`section-07`}/>
-            case 8: return <Section05 key={`section-08`}/>
-            case 9: return <Section05 key={`section-09`}/>
-            case 10: return <Section05 key={`section-10`}/>
-            case 11: return <Section05 key={`section-11`}/>
+            case 6: return <Section06 key={`section-06`}/>
+            case 7: return <Section07 key={`section-07`}/>
+            case 8: return <Section08 key={`section-08`}/>
+            case 9: return <Section09 key={`section-09`}/>
+            case 10: return <Section10 key={`section-10`}/>
+            case 11: return <Section11 key={`section-11`}/>
             default: return formatText(this.getTestSection(100), "section --temporal")
         }
     }
