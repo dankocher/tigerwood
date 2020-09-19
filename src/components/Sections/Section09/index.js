218d56b4f6dd6e10b669381cadee9298bdabfedd
Daniel Chernyshev <droque123@gmail.com>
2020-09-19 15:15:55 +0300

    Section 9

--- src/components/Sections/Section09/index.js
+++ src/components/Sections/Section09/index.js
@@ -12,10 +12,35 @@ export default class Section extends React.Component {
         return (
             <div className={`section --s_${SECTION_NUMBER}`}>
                 <div className={`--s_${SECTION_NUMBER}-content`}>
+                    <div className="-s9-text">
+                        <div className="-header-1">{t.header_1}</div>
+                        <div className="-header-2">{t.header_2}</div>
+                        <div className="delivery-text">
+                            <div className="delivery-title">{t.delivery.title}</div>
+                            <ul className="delivery-features">{
+                                t.delivery.features.map((f, i) => (
+                                    <li key={i} className="-f">{f}</li>
+                                ))
+                            }</ul>
 
+                            <div className="-pickup">
+                                <div className="pickup-title">{formatText(t.pickup)}</div>
+                                <div className="pickup-address">{t.pickup_address}</div>
+                            </div>
+
+                            <div className="delivery-title --russia">{t.delivery_russia.title}</div>
+                            <ul className="delivery-features --russia">{
+                                t.delivery_russia.features.map((f, i) => (
+                                    <li key={i} className="-f">{f}</li>
+                                ))
+                            }</ul>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
     }
 }
+
+
 
