8cf585f1311c9b9413dcef46f6dd19785111f89f
Daniel Chernyshev <droque123@gmail.com>
2020-09-23 20:57:44 +0300

    Section 6 mobile

--- src/components/Sections/Section06/index.js
+++ src/components/Sections/Section06/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import "./styles.scss";
 import t from "./text.json";
-import formatText from "../../../utils/formatText";
+
+import image from "./images/s5-video.jpg";
 
 const SECTION_NUMBER = "06"
 
@@ -15,10 +16,11 @@ export default class Section extends React.Component {
                     <div className="-s06-header">
                         <div className="-s06-header-1">{t.header_1}</div>
                         <div className="-s06-header-2">{t.header_2}</div>
-                        <div className="-s06-header-2">{t.header_3}</div>
+                        <div className="-s06-header-3">{t.header_3}</div>
                     </div>
                     <div className="-s5-video">
                         <div className="-video-content">
+                            <img src={image} alt=""/>
                             <div className="-play-button"/>
                         </div>
                     </div>
