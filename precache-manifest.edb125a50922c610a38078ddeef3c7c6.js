0d71ba46c93ba43d5614f58ee3776c04a889a607
Daniel Chernyshev <droque123@gmail.com>
2020-09-25 15:50:03 +0300

    Updates

---
+++ precache-manifest.edb125a50922c610a38078ddeef3c7c6.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e838b6227e71e7e21195aee900513a15",
+    "url": "/tigerwood/index.html"
+  },
+  {
+    "revision": "fc9238113e9c07baa2be",
+    "url": "/tigerwood/static/css/2.1925a4ac.chunk.css"
+  },
+  {
+    "revision": "37b2b55ffd9ede45743c",
+    "url": "/tigerwood/static/css/main.ca94cf28.chunk.css"
+  },
+  {
+    "revision": "fc9238113e9c07baa2be",
+    "url": "/tigerwood/static/js/2.081b18a5.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/tigerwood/static/js/2.081b18a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37b2b55ffd9ede45743c",
+    "url": "/tigerwood/static/js/main.0febf7c9.chunk.js"
+  },
+  {
+    "revision": "7e17d95cef2deb6e6f15",
+    "url": "/tigerwood/static/js/runtime-main.338527c1.js"
+  },
+  {
+    "revision": "ade91f473255991f410f61857696434b",
+    "url": "/tigerwood/static/media/Montserrat-Bold.ade91f47.ttf"
+  },
+  {
+    "revision": "6abf597a1f3f44fd10bdcb7b86b494f0",
+    "url": "/tigerwood/static/media/Nunito-Black.6abf597a.ttf"
+  },
+  {
+    "revision": "1cd294a771f26752bbb8d8d5210f6412",
+    "url": "/tigerwood/static/media/Nunito-Bold.1cd294a7.ttf"
+  },
+  {
+    "revision": "19caf1c06e022593ea4657734d691c86",
+    "url": "/tigerwood/static/media/Nunito-ExtraBold.19caf1c0.ttf"
+  },
+  {
+    "revision": "d2e691bc4a2b696929172cb3d22ce8ba",
+    "url": "/tigerwood/static/media/Nunito-Regular.d2e691bc.ttf"
+  },
+  {
+    "revision": "76ae20d4d4b7d46e613d2581512fcf73",
+    "url": "/tigerwood/static/media/Nunito-SemiBold.76ae20d4.ttf"
+  },
+  {
+    "revision": "c54bbd1ed30665a0f48bf56792970847",
+    "url": "/tigerwood/static/media/bg.c54bbd1e.svg"
+  },
+  {
+    "revision": "a5693a2ec25eeb235da069d8b818c193",
+    "url": "/tigerwood/static/media/icon-bg.a5693a2e.svg"
+  },
+  {
+    "revision": "d51c4d2786f7af60d813c14615666ff5",
+    "url": "/tigerwood/static/media/icon-bg.d51c4d27.svg"
+  },
+  {
+    "revision": "dc5ce830d236a2c189153f296fb558cf",
+    "url": "/tigerwood/static/media/icon-bg.dc5ce830.svg"
+  },
+  {
+    "revision": "46c7ee995b30d186fb3e16c9659cae4b",
+    "url": "/tigerwood/static/media/play.46c7ee99.svg"
+  },
+  {
+    "revision": "26d1276c89c121ace00a4e70cb16127b",
+    "url": "/tigerwood/static/media/product-01.26d1276c.png"
+  },
+  {
+    "revision": "f992f3ddbb492ffa480bdf4c36c8d211",
+    "url": "/tigerwood/static/media/product-1.f992f3dd.png"
+  },
+  {
+    "revision": "4fbcfc46b672d873e4f5e5a038f79453",
+    "url": "/tigerwood/static/media/product-wide-1.4fbcfc46.jpg"
+  },
+  {
+    "revision": "436fa1c32fd21e5c60c6fec26aabd57d",
+    "url": "/tigerwood/static/media/review-1.436fa1c3.jpg"
+  },
+  {
+    "revision": "667b85c9a9dd6edd882fa5d16409df2a",
+    "url": "/tigerwood/static/media/s2-bg.667b85c9.png"
+  },
+  {
+    "revision": "4d1a00f27c8143f9dff0636459309e0f",
+    "url": "/tigerwood/static/media/s4-bg.4d1a00f2.png"
+  },
+  {
+    "revision": "13a8b047bfad3dc5f0a54084b8271e17",
+    "url": "/tigerwood/static/media/s4-picture.13a8b047.png"
+  },
+  {
+    "revision": "cc8798360dd8c63bb5b46fc620ed7114",
+    "url": "/tigerwood/static/media/s5-video.cc879836.jpg"
+  },
+  {
+    "revision": "af390c04bb6db79a0e0f8099121f77cd",
+    "url": "/tigerwood/static/media/s9-bg.af390c04.png"
+  },
+  {
+    "revision": "ce546803a74610a3f2085dbd02c1f5e2",
+    "url": "/tigerwood/static/media/section1-bg.ce546803.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/tigerwood/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/tigerwood/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/tigerwood/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/tigerwood/static/media/slick.f97e3bbf.svg"
+  }
+]);
