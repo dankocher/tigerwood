f13685bd4b053d46e238662ced4aaf7eb38d6812
Daniel <droque123@gmail.com>
2021-03-05 00:13:41 +0300

    Fixed headers

--- public/adminapi/headers.php
+++ public/adminapi/headers.php
@@ -1,5 +1,5 @@
 <?php
 
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: http://192.168.100.11:3000");
 header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
