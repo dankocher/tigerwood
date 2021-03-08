576d7e0beec546267ef09284f94c8ae5a1c31c5c
Daniel <droque123@gmail.com>
2021-03-08 16:01:15 +0300

    Save api to temp folder

--- public/update/update_site.php
+++ public/update/update_site.php
@@ -9,6 +9,7 @@ if ($pass === sha1($_REQUEST['pass'])) {
 
     $zip = new ZipArchive;
     if ($zip->open('build.zip') === TRUE) {
+        xcopy("../api", "./temp/api/");
         $zip->extractTo('files/');
         $zip->close();
         echo 'Ready<br>';
@@ -21,3 +22,68 @@ if ($pass === sha1($_REQUEST['pass'])) {
 unlink("build.zip");
 echo "<a href='/update'>Обновить еще раз</a><br>";
 echo "<a href='/'>На главную</a>";
+
+
+
+
+
+
+
+
+
+function xcopy($source, $dest, $permissions = 0755)
+{
+    $sourceHash = hashDirectory($source);
+    // Check for symlinks
+    if (is_link($source)) {
+        return symlink(readlink($source), $dest);
+    }
+
+    // Simple copy for a file
+    if (is_file($source)) {
+        return copy($source, $dest);
+    }
+
+    // Make destination directory
+    if (!is_dir($dest)) {
+        mkdir($dest, $permissions);
+    }
+
+    // Loop through the folder
+    $dir = dir($source);
+    while (false !== $entry = $dir->read()) {
+        // Skip pointers
+        if ($entry == '.' || $entry == '..') {
+            continue;
+        }
+
+        // Deep copy directories
+        if($sourceHash != hashDirectory($source."/".$entry)){
+             xcopy("$source/$entry", "$dest/$entry", $permissions);
+        }
+    }
+
+    // Clean up
+    $dir->close();
+    return true;
+}
+
+// In case of coping a directory inside itself, there is a need to hash check the directory otherwise and infinite loop of coping is generated
+
+function hashDirectory($directory){
+    if (! is_dir($directory)){ return false; }
+
+    $files = array();
+    $dir = dir($directory);
+
+    while (false !== ($file = $dir->read())){
+        if ($file != '.' and $file != '..') {
+            if (is_dir($directory . '/' . $file)) { $files[] = hashDirectory($directory . '/' . $file); }
+            else { $files[] = md5_file($directory . '/' . $file); }
+        }
+    }
+
+    $dir->close();
+
+    return md5(implode('', $files));
+}
