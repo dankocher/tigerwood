76b692ca41f12ef55273e0c3cbee6b7a09c90d50
Daniel Roke <droque123@gmail.com>
2021-06-20 18:53:45 +0300

    Fixed update

--- public/update/index.php
+++ public/update/index.php
@@ -5,6 +5,7 @@ echo '
 <form enctype="multipart/form-data" action="/update/update_site.php" method="POST">
     <input type="hidden" name="512mb" value="30000" /><br>
     Загрузите файл <b>buid.zip</b>: <input name="build" type="file" /><br>
+    exclude <b>api, update, adminapi</b><br>
     Ведите пароль администратора: <input name="pass" type="password" /><br>
     <input type="submit" value="Отправить файл" />
 </form>
